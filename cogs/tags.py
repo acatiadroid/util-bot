daeb4743cdac04fdb3bc0849a07357a94bd6101a
Luke <69216256+acatiadroid@users.noreply.github.com>
2021-05-02 15:37:28 +0100

    clean_content and lower() for creating tags

--- cogs/tags.py
+++ cogs/tags.py
@@ -2,21 +2,22 @@ import discord
 from datetime import datetime
 from discord.ext import commands
 
+
 class Tags(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
         pass
 
-    @commands.group(invoke_without_command=True, aliases = ['t'])
-    async def tag(self, ctx, *, tag=None):
+    @commands.group(invoke_without_command=True, aliases=['t'])
+    async def tag(self, ctx, *, tag: commands.clean_content=None):
         """Performs a tag."""
         data = await self.bot.tags.find(tag)
         if not data:
             return await ctx.send('Unable to find a tag with that name.')
-        
+
         try:
             await ctx.send(data["content"], allowed_mentions=None)
             if "delInvoke" in data:
@@ -24,18 +25,23 @@ class Tags(commands.Cog):
                     await ctx.message.delete()
         except Exception as e:
             await ctx.send(f'Failed to send tag: ```{e}```', allowed_mentions=None)
-    
-    @tag.command(aliases = ['+'])
-    async def create(self, ctx, tagname, *, content):
+
+    @tag.command(aliases=['+'])
+    async def create(self, ctx, tagname: commands.clean_content, *, content: commands.clean_content):
         """Creates a new tag."""
-        notag = discord.utils.get(ctx.guild.roles, name = 'No Tags')
+        notag = discord.utils.get(ctx.guild.roles, name='No Tags')
         if notag in ctx.author.roles:
             return await ctx.send('You can\'t do this.')
-        
+
         data = await self.bot.tags.find(tagname)
         if data:
             return await ctx.send('A tag with that name already exists.')
 
+        if tagname != tagname.lower():
+            tagname = tagname.lower()
+            note = "(the tag has been created all in lowercase.)"
+        else:
+            note = None
         try:
             await self.bot.tags.upsert(
                 {
@@ -46,16 +52,16 @@ class Tags(commands.Cog):
                     "guild": ctx.guild.id,
                 }
             )
-            await ctx.send(f'**{tagname}** successfully created. :ok_hand:', allowed_mentions=None)
-            
+            await ctx.send(f'**{tagname}** successfully created. :ok_hand: {note}', allowed_mentions=None)
+
         except Exception as e:
             return await ctx.send('Failed to create that tag. ```{}```'.format(e))
 
-    @tag.command(aliases = ['-'])
-    async def delete(self, ctx, *, tagname):
+    @tag.command(aliases=['-'])
+    async def delete(self, ctx, *, tagname: commands.clean_content):
         """Deletes a tag. You must be the tag creator or a moderator to delete tags."""
         data = await self.bot.tags.find(tagname)
-        mrole = discord.utils.get(ctx.guild.roles, name = 'Moderator')
+        mrole = discord.utils.get(ctx.guild.roles, name='Moderator')
         if not data:
             return await ctx.send('Unable to find a tag with that name.')
 
@@ -65,23 +71,23 @@ class Tags(commands.Cog):
             pass
         else:
             return await ctx.send('You can\'t do this. You must either be the tag creator or be a moderator.')
-        
+
         await self.bot.tags.delete_by_id(tagname)
 
         await ctx.send(f'**{tagname}** successfully deleted. :ok_hand:', allowed_mentions=None)
 
-    @tag.command(aliases = ['delinvoke', 'di'])
-    async def delete_invoke(self, ctx, *, tagname):
+    @tag.command(aliases=['delinvoke', 'di'])
+    async def delete_invoke(self, ctx, *, tagname: commands.clean_content):
         """Toggles deleting the invocation of a certain tag. You must either be the tag creator or a moderator to do this."""
         data = await self.bot.tags.find(tagname)
-        modrole = discord.utils.get(ctx.guild.roles, name = 'Moderator')
+        modrole = discord.utils.get(ctx.guild.roles, name='Moderator')
         if not data:
             return await ctx.send('Unable to find a tag with that name.')
-        
+
         if data["createdBy"] != ctx.author or data["createdBy"] != ctx.author.id:
             if modrole not in ctx.author.roles:
                 return await ctx.send('You can\'t do this to tags that you don\'t own.')
-        
+
         if "delInvoke" in data:
             if data["delInvoke"] == True:
                 await self.bot.tags.upsert({"_id": str(tagname), "delInvoke": False})
@@ -90,8 +96,8 @@ class Tags(commands.Cog):
 
         await ctx.send(f'The message invocation to **{tagname}** will now be deleted. :ok_hand:')
 
-    @tag.command(aliases = ['i'])
-    async def info(self, ctx, *, tagname):
+    @tag.command(aliases=['i'])
+    async def info(self, ctx, *, tagname: commands.clean_content):
         """Shows info on a certain tag."""
         data = await self.bot.tags.find(tagname)
         if not data:
@@ -100,25 +106,25 @@ class Tags(commands.Cog):
         length = len(data["content"])
 
         e = discord.Embed(
-            title = data["_id"],
-            description = f'Created: {data["createdAt"]} UTC\nCreator: {data["createdBy"]}\nTag content length: {length}',
-            color = discord.Color.blue()
+            title=data["_id"],
+            description=f'Created: {data["createdAt"]} UTC\nCreator: {data["createdBy"]}\nTag content length: {length}',
+            color=discord.Color.blue()
         )
-        await ctx.send(embed=e) 
-    
-    @tag.command(name='list', aliases = ['all'])
+        await ctx.send(embed=e)
+
+    @tag.command(name='list', aliases=['all'])
     async def _list(self, ctx):
         """Lists all of the current tags."""
         data = await self.bot.tags.get_all()
         e = discord.Embed(
-            title = 'All tags',
-            description = ", ".join([str(d.get("_id")) for d in data]),
-            color = discord.Color.blue()
+            title='All tags',
+            description=", ".join([str(d.get("_id")) for d in data]),
+            color=discord.Color.blue()
         )
         await ctx.send(embed=e)
 
     @tag.command(name='edit', aliases=['e'])
-    async def _edit(self, ctx, tagname, *, content):
+    async def _edit(self, ctx, tagname: commands.clean_content, *, content: commands.clean_content):
         """Edits the content of a tag.
 
         You can use ?tag raw to get the raw content of an existing tag.
@@ -129,16 +135,17 @@ class Tags(commands.Cog):
 
         modrole = ctx.guild.get_role(723237557193670742)
 
-        if data["createdBy"] != ctx.author or data["createdBy"] != ctx.author.id and modrole not in ctx.author.roles:
-            return await ctx.send('You don\'t own this tag.')
+        if modrole not in ctx.author.roles:
+            if data["createdBy"] != ctx.author:
+                if data["createdBy"] != ctx.author.id:
+                    return await ctx.send('You don\'t own this tag.')
 
         await self.bot.tags.upsert({"_id": tagname, "content": str(content)})
         await ctx.send(f'**{tagname}** successfully edited. :ok_hand:')
 
-    
-    @tag.command(name = 'block')
+    @tag.command(name='block')
     @commands.has_role('Moderator')
-    async def _block(self, ctx, *, member:discord.Member):
+    async def _block(self, ctx, *, member: discord.Member):
         role = ctx.guild.get_role(815313621835841587)
         if role in member.roles:
             await member.remove_roles(role)
@@ -149,18 +156,19 @@ class Tags(commands.Cog):
             await ctx.send('Can\'t do that. ```{e}```'.format(e))
 
     @tag.command(name='raw')
-    async def _raw(self, ctx, *, tagname):
+    async def _raw(self, ctx, *, tagname: commands.clean_content):
         """Gets the raw content of a message. This is useful for editing tags."""
         data = await self.bot.tags.find(tagname)
         if not data:
             return await ctx.send('Unable to find a tag with that name.')
-        
+
         await ctx.send(f"```{data['content']}```")
 
     @tag.error
     async def tag_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
-            return await ctx.send('Missing a required argument.')    
-    
+            return await ctx.send('Missing a required argument.')
+
+
 def setup(bot):
     bot.add_cog(Tags(bot))
