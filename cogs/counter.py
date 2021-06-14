50877d491c88089936ff5641ec1e07538ee4b257
Luke <69216256+acatiadroid@users.noreply.github.com>
2021-06-14 20:46:20 +0100

    change GUILD_ID to COUNTING_CHANNEL

--- cogs/counter.py
+++ cogs/counter.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 
-from utils.secrets import GUILD_ID
+from utils.secrets import GUILD_ID, COUNTING_CHANNEL
+
 
 class Counter(commands.Cog):
     def __init__(self, bot):
@@ -9,15 +10,16 @@ class Counter(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.id != GUILD_ID:
+        if message.channel.id != COUNTING_CHANNEL:
             return
+
+        print('1')
 
         try:
             message.content.isdecimal()
         except TypeError:
             await message.author.send('Incorrect number. Do `?count` to see the current number.')
             await message.delete()
-
 
         data = await self.bot.counter.find(GUILD_ID)
         if not data:
@@ -46,11 +48,11 @@ class Counter(commands.Cog):
 
     @commands.command()
     @commands.has_role('Moderator')
-    async def cantcount(self, ctx, *, member:discord.Member):
-        role = discord.utils.get(ctx.guild.roles, name = "Can't count")
+    async def cantcount(self, ctx, *, member: discord.Member):
+        role = discord.utils.get(ctx.guild.roles, name="Can't count")
         await member.add_roles(role)
         await ctx.send(f'**{member}** doesn\'t have the mental capacity to count, so they won\'t be able to anymore...')
 
+
 def setup(bot):
     bot.add_cog(Counter(bot))
-
